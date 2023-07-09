658fef637215f2fcfd52d7419e19bb87c96a6baa
Pili L <mariapilar.lopezgracia@grupocarreras.com>
2023-07-09 15:21:25 +0200

    Rename tests

--- src/test/java/com/capitole/tecnicalTest/OrdermanagmentTest.java
+++ src/test/java/com/capitole/tecnicalTest/OrdermanagmentTest.java
@@ -17,14 +17,22 @@ public class OrdermanagmentTest extends AbstractTest {
 
 
     @Test
-    public void getMaxOrderIdByUser_t001(){
+    public void getLastOrderForEachUserWithoutCondictions_resultEmptyList(){
         OrderSearchCriteria criteria = new OrderSearchCriteria();
         List<HashMap> result = this.ordermanagment.getMaxOrderIdByUser(criteria);
         Assert.assertTrue("OK ", result.size()>1);
     }
 
     @Test
-    public void getMaxOrderIdByUser_t002(){
+    public void getLastOrderForEachUserWithStoreCondiction_StoreDoesExist_resultListWithElements(){
+        OrderSearchCriteria criteria = new OrderSearchCriteria();
+        criteria.setIdStore(1L);
+        List<HashMap> result = this.ordermanagment.getMaxOrderIdByUser(criteria);
+        Assert.assertTrue("OK ", result.size()>0);
+    }
+
+    @Test
+    public void getLastOrderForEachUserWithStoreCondiction_StoreDoesNotExist_resultListWithElements(){
         OrderSearchCriteria criteria = new OrderSearchCriteria();
         criteria.setIdStore(55L);
         List<HashMap> result = this.ordermanagment.getMaxOrderIdByUser(criteria);
@@ -32,21 +40,21 @@ public class OrdermanagmentTest extends AbstractTest {
     }
 
     @Test
-    public void getOrderById_t001(){
+    public void getOrderWhenidOrderExist_resultOrderWithSameIdThatsearchedOrder(){
         Long idOrder = 1L;
         OrderTo result = this.ordermanagment.getOrderById(idOrder);
         Assert.assertTrue("OK ", result != null && idOrder.equals(result.getIdOrder()));
     }
 
     @Test
-    public void getOrderById_t002(){
+    public void getOrderDoesNotExist_resultNullObject(){
         Long idOrder = 55L;
         OrderTo result = this.ordermanagment.getOrderById(idOrder);
         Assert.assertTrue("OK ", result == null);
     }
 
     @Test
-    public void copyOrdersFromUserToUser_t001(){
+    public void copyOrdersFromUserDoesExistToUserDoesExist_resultSameNumberOfOrderOfUserTo(){
 
         Long idUserFrom = 1L;
         Long idUserTo = 4L;
@@ -67,6 +75,30 @@ public class OrdermanagmentTest extends AbstractTest {
         Assert.assertTrue("OK ",
                 orderUserToAfterCopyList.size() == (orderUserFromList.size() + orderUserToList.size()));
 
+
+    }
+
+    @Test
+    public void copyOrders_FromUserdoesNotExistToUserDoesExist_resultSameNumberOfOrderOfUserTo(){
+
+        Long idUserFrom = 55L;
+        Long idUserTo = 4L;
+
+        OrderSearchCriteria criteriaUserFrom = new OrderSearchCriteria();
+        criteriaUserFrom.setIdUser(idUserFrom);
+
+        OrderSearchCriteria criteriaUserTo = new OrderSearchCriteria();
+        criteriaUserTo.setIdUser(idUserTo);
+
+        List<OrderTo> orderUserFromList = this.ordermanagment.findAll(criteriaUserFrom);
+        List<OrderTo> orderUserToList = this.ordermanagment.findAll(criteriaUserTo);
+
+        this.ordermanagment.copyOrdersFromUserToUser(idUserFrom, idUserTo);
+
+        List<OrderTo> orderUserToAfterCopyList = this.ordermanagment.findAll(criteriaUserTo);
+
+        Assert.assertTrue("OK ",
+                orderUserToAfterCopyList.size() ==  orderUserToList.size());
 
     }
 
